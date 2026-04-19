e919b5689c9f5ba1dc79a9bd48e6f108da2dc974
AminduUK <aminduumayangakule23@gmail.com>
2026-04-19 13:35:21 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/MovieRentalAndReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.movie_rental_and_review_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRentalAndReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRentalAndReviewPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ac/sliit/movie_rental_and_review_platform/MovieRentalAndReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.movie_rental_and_review_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRentalAndReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
