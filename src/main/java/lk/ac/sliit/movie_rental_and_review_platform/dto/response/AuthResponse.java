00b7822aabf680174aa20a41d480d27c05ed9cea
AminduUK <aminduumayangakule23@gmail.com>
2026-04-19 14:07:11 +0530

    Create DTO for user sign-up & sign-in

---
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/dto/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package lk.ac.sliit.movie_rental_and_review_platform.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String role;
+    private String name;
+}
