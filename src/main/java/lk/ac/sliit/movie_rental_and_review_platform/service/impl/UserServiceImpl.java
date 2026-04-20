a0a23e2d1c38b9a54f8e3046a14915af2950d27b
AminduUK <aminduumayangakule23@gmail.com>
2026-04-20 22:24:40 +0530

    Implement get-user methods

--- src/main/java/lk/ac/sliit/movie_rental_and_review_platform/service/impl/UserServiceImpl.java
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/service/impl/UserServiceImpl.java
@@ -3,30 +3,35 @@ package lk.ac.sliit.movie_rental_and_review_platform.service.impl;
 import lk.ac.sliit.movie_rental_and_review_platform.dto.request.SignInRequest;
 import lk.ac.sliit.movie_rental_and_review_platform.dto.request.SignUpRequest;
 import lk.ac.sliit.movie_rental_and_review_platform.dto.response.AuthResponse;
+import lk.ac.sliit.movie_rental_and_review_platform.dto.response.UserResponse;
 import lk.ac.sliit.movie_rental_and_review_platform.entity.UserEntity;
 import lk.ac.sliit.movie_rental_and_review_platform.enums.Role;
 import lk.ac.sliit.movie_rental_and_review_platform.repository.UserRepository;
 import lk.ac.sliit.movie_rental_and_review_platform.security.JwtUtil;
 import lk.ac.sliit.movie_rental_and_review_platform.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userDao;
-    private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
+    private final ModelMapper modelMapper;
 
+    @Override
     public AuthResponse signup(SignUpRequest request) {
-        if (userDao.existsByEmail(request.getEmail())) {
+        if (userRepository.existsByEmail(request.getEmail())) {
             throw new RuntimeException("Email already registered");
         }
 
@@ -37,21 +42,51 @@ public class UserServiceImpl implements UserService {
         user.setRole(Role.valueOf(request.getRole()));
         user.setCreatedDate(new Date());
 
-        userDao.save(user);
+        userRepository.save(user);
 
         String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
         return new AuthResponse(token, user.getRole().name(), user.getName());
     }
 
+    @Override
     public AuthResponse signin(SignInRequest request) {
         // This throws an exception automatically if credentials are wrong
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
         );
 
-        UserEntity user = userDao.findByEmail(request.getEmail()).get();
+        UserEntity user = userRepository.findByEmail(request.getEmail()).get();
         String token = jwtUtil.generateToken(user.getEmail(), user.getRole().name());
         return new AuthResponse(token, user.getRole().name(), user.getName());
+    }
+
+    @Override
+    public List<UserResponse> getAllUsers() {
+        List<UserResponse> userResponseList = new ArrayList<>();
+
+        userRepository.findAll().forEach(userEntity -> {
+            if (userEntity.getRole() == Role.ROLE_USER) {
+                UserResponse userResponse = new UserResponse();
+                userResponse.setUserID(userEntity.getUserID());
+                userResponse.setName(userEntity.getName());
+                userResponse.setEmail(userEntity.getEmail());
+                userResponse.setCreatedDate(userEntity.getCreatedDate());
+                userResponseList.add(userResponse);
+            }
+        });
+        return userResponseList;
+    }
+
+    @Override
+    public UserResponse getUser(String email) {
+        UserEntity userEntity = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found or is not a regular user"));
+        UserResponse userResponse = new UserResponse();
+        userResponse.setUserID(userEntity.getUserID());
+        userResponse.setName(userEntity.getName());
+        userResponse.setEmail(userEntity.getEmail());
+        userResponse.setCreatedDate(userEntity.getCreatedDate());
+        return userResponse;
     }
 
 }
