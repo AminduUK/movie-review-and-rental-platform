1b0673489dae40228517def2c04814b4aec3e3e8
Charuka147 <dilipcharuka147@gmail.com>
2026-04-25 19:16:48 +0530

    Create new PaymentEntity

--- src/main/java/lk/ac/sliit/movie_rental_and_review_platform/entity/PaymentEntity.java
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/entity/PaymentEntity.java
@@ -1,8 +1,6 @@
 package lk.ac.sliit.movie_rental_and_review_platform.entity;
 
 import jakarta.persistence.*;
-import lk.ac.sliit.movie_rental_and_review_platform.enums.PaymentMethod;
-import lk.ac.sliit.movie_rental_and_review_platform.enums.PaymentStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,28 +11,38 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "payments")
+@Table(name = "payment")
 public class PaymentEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long paymentId;
 
+    @Column(nullable = false)
     private Double amount;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentMethod paymentMethod;
 
+    public enum PaymentMethod {
+        CARD, CASH, UPI
+    }
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PaymentStatus status;
 
+    public enum PaymentStatus {
+        SUCCESS, FAILED
+    }
+
+    @Column(nullable = false)
     private LocalDateTime paymentDate;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private UserEntity user;
+    // One-to-One → RENTAL (owns the FK)
+//    @OneToOne
+//    @JoinColumn(name = "rental_id", nullable = false, unique = true)
+//    private RentalEntity rental;
 
-    @OneToOne
-    @JoinColumn(name = "rental_id", unique = true)
-    private RentalEntity rental;
 }
