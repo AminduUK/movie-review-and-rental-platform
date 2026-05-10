673929b6881e0182dc315107c056b910ff6d3cb6
Charuka147 <dilipcharuka147@gmail.com>
2026-05-10 13:35:42 +0530

    Update Rental Service Package

--- src/main/java/lk/ac/sliit/movie_rental_and_review_platform/service/RentalService.java
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/service/RentalService.java
@@ -2,9 +2,16 @@ package lk.ac.sliit.movie_rental_and_review_platform.service;
 
 import lk.ac.sliit.movie_rental_and_review_platform.dto.request.rental.CreateRentalRequest;
 import lk.ac.sliit.movie_rental_and_review_platform.dto.response.rental.RentalResponse;
+import java.util.List;
 
 public interface RentalService {
 
     RentalResponse rentMovie(Long userId, CreateRentalRequest createRequest);
+
+    List<RentalResponse> getActiveRentals(Long userId);
+
+    List<RentalResponse> getRentalHistory(Long userId);
+
+    RentalResponse returnMovie(Long userId, Long rentalId);
 
 }
