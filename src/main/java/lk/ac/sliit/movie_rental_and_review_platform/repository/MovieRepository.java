421d7589ebe521afd356183d34ac2a7086e2d730
pgsavidu <pgsavidu@gmail.com>
2026-05-11 16:22:55 +0530

    Update Movie Repository

--- src/main/java/lk/ac/sliit/movie_rental_and_review_platform/repository/MovieRepository.java
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/repository/MovieRepository.java
@@ -3,10 +3,17 @@ package lk.ac.sliit.movie_rental_and_review_platform.repository;
 import lk.ac.sliit.movie_rental_and_review_platform.entity.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
 
     boolean existsByTitleIgnoreCase(String title);
+
+    List<MovieEntity> findByTitleContainingIgnoreCase(String title);
+
+    List<MovieEntity> findByCategoriesCategoryId(Long categoryId);
+
+    List<MovieEntity> findByReleaseYear(Integer releaseYear);
 
 }
