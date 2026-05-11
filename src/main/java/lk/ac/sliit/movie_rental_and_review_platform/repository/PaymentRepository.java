86ec991504518765b7319aa9d6abf292ab5d3d9a
Charuka147 <dilipcharuka147@gmail.com>
2026-05-11 16:51:08 +0530

    Update PaymentRepository

--- src/main/java/lk/ac/sliit/movie_rental_and_review_platform/repository/PaymentRepository.java
+++ src/main/java/lk/ac/sliit/movie_rental_and_review_platform/repository/PaymentRepository.java
@@ -2,7 +2,9 @@ package lk.ac.sliit.movie_rental_and_review_platform.repository;
 
 import lk.ac.sliit.movie_rental_and_review_platform.entity.PaymentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
 
 }
